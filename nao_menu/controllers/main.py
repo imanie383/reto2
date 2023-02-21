155329a5fc220b2549452f530534964c09327aa3
imanie383 <manuel@vauxoo.com>
2023-02-20 23:41:25 -0600

    [ADD] nao_menu: Validaitons and Notifications

--- nao_menu/controllers/main.py
+++ nao_menu/controllers/main.py
@@ -1,9 +1,15 @@
 # -*- coding: utf-8 -*-
 # Part of Odoo. See LICENSE file for full copyright and licensing details.
 
-from odoo import http, _
+from odoo import http, fields, _
 from odoo.exceptions import AccessError, MissingError
 from odoo.http import request
+
+from pushbullet import PushBullet
+from pywebio.input import *
+from pywebio.output import *
+from pywebio.session import *
+import time
 
 
 class MenuController(odoo.http.Controller):
@@ -14,10 +20,27 @@ class MenuController(odoo.http.Controller):
 
     @http.route(['/employee/<model("nao.employee"):employee/menu'], type='json', auth="public", methods=['GET'], website=True)
     def employee_get_menu(self, employee, **kw):
+        today = fields.Date.today()
+        now = fields.datetime.now()
+        limit = now.replace(hour=11, minute=0, second=0, microsecond=0)
+
+        if today.weekday() != 0 or now > limit:
+            return {
+                'message': "Solo puedes reservar los lunes antes de las 11 AM",
+                'success': False,
+            }
+
         menus = request.env['nao.menu.plate'].search([
             ('level_id', '=', employee.level_id)
         ], order='category_id')
-        return menus.read()
+
+
+        return {
+            'message': "Exito",
+            'success': True,
+            'menus': menus.read(),
+        }
+
 
     @http.route(['/employee/<model("nao.employee"):employee/menu'], type='json', auth="public", methods=['POST'], website=True)
     def employee_get_menu(self, employee, plates, **kw):
@@ -33,7 +56,17 @@ class MenuController(odoo.http.Controller):
                 'plate_id': plate['id'],
             })
 
+        message = "Hola %s, ¡tu orden ha sido recibida!" % employee.id
+        this.notificacion(message)
+
         return {
-            'message': "Hola %s, ¡tu orden ha sido recibida!" % employee.id,
+            'message': message,
             'success': True,
         }
+
+    def notificacion(self, message=''):
+        access_token = "o.q1QIkYnI2WUdNX3wPSiWSp"
+        title = input('Title')
+        pb = PushBullet(access_token)
+        push = pb.push_note(title, message)
+        hold()
